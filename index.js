14d71db36657fdef66af0e898bb3d7bdb69aa2a2
zahara-Rahmanchy <zaharachy72@gmail.com>
2023-05-02 21:00:52 +0600

    chefs data sent using res

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 5000;
-
+const chefs = require("./Data/chefdata.json");
 app.get("/", (req, res) => {
   res.send("Flavour Adventure Running");
 });
 
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
+});
 app.listen(port, () => {
   console.log("port:500");
 });
